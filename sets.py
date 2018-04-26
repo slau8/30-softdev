c529b214734e87bada00b556d3e816da92cf8d3b
slau8 <slau8@stuy.edu>
2018-04-26 09:15:43 -0400

    done??

---
+++ sets.py
@@ -0,0 +1,23 @@
+def union(a,b):
+    return set_difference(a,b) + b
+
+def intersection(a,b):
+    return [i for i in a if i in b]
+
+def set_difference(a,b):
+    return [i for i in a if i not in b]
+
+def symmetric_difference(a,b):
+    return union(set_difference(a,b),set_difference(b,a))
+
+def cartesian_product(a,b):
+    return [(i,j) for i in a for j in b]
+
+a = [1,2,3]
+b = [2,3,4]
+
+print union(a,b)
+print intersection(a,b)
+print set_difference(a,b)
+print symmetric_difference(a,b)
+print cartesian_product(a,b)
